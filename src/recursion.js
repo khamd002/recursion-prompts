b8318316b3924577d6516c25c22c43a00e8bdb60
Patty Kovash <pattykovash@gmail.com>
2017-11-18 13:25:08 -0700

    Pass Sum of Integers

--- src/recursion.js
+++ src/recursion.js
@@ -19,6 +19,11 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  var temp = array.slice();
+  if (array.length === 0) {
+    return 0;
+  }
+  return temp.shift() + sum(temp);
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
