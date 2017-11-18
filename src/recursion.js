e5c79779cbc6dc1b58490b4b375c4c77b872bd7a
Patty Kovash <pattykovash@gmail.com>
2017-11-18 13:52:03 -0700

    Pass Check if Even

--- src/recursion.js
+++ src/recursion.js
@@ -42,6 +42,17 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  if (n === 0) {
+    return true;
+  }
+  if (n === -1) {
+    return false;
+  }
+  if (n < 0) {
+    return isEven(n + 2);
+  }
+  return isEven(n - 2);
+
 };
 
 // 5. Sum all integers below a given integer.
