575194cd8e01b45ae3cc003d45c46271d1867481
Patty Kovash <pattykovash@gmail.com>
2017-11-18 22:12:58 -0700

    Pass Power of Two

--- src/recursion.js
+++ src/recursion.js
@@ -107,10 +107,18 @@ var exponent = function(base, exp) {
 // powerOfTwo(16); // true
 // powerOfTwo(10); // false
 var powerOfTwo = function(n) {
+  if (n === 1) {
+    return true;
+  }
+  if (n <= 0) {
+    return false;
+  }
+  return powerOfTwo(n / 2);
 };
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+
 };
 
 // 10. Write a function that determines if a string is a palindrome.
