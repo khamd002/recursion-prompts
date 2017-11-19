61f126dc589873fe0b3546ee22f4f0c0a937054e
Patty Kovash <pattykovash@gmail.com>
2017-11-18 17:20:33 -0700

    Pass Integer Range

--- src/recursion.js
+++ src/recursion.js
@@ -71,6 +71,20 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+  var result = [];
+  if (x < y) {
+    if (x + 1 >= y) {
+      return result;
+    }
+    result.push(x + 1);
+    return result.concat(range(x + 1, y));
+  } else {
+    if (x - 1 <= y) {
+      return result;
+    }
+    result.push(x - 1);
+    return result.concat(range(x - 1, y));
+  }
 };
 
 // 7. Compute the exponent of a number.
